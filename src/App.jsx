2dca2f343f2828c12bf7d703e838d06456e48b1d
Ashiq <web3.0.ashiq@gmail.com>
2023-03-13 19:00:38 +0600

    hero banner add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getApiConfiguration } from './store/homeSlice';
 import { fetchDataFromApi } from './utils/api'
+
 import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 
 function App() {
@@ -12,23 +19,36 @@ function App() {
   const {url} = useSelector((state)=> state.home)
 
   useEffect(()=>{
-  apiTesting()
+  fetchApiConfig()
 },[]);
 
-  const apiTesting =()=> {
-    fetchDataFromApi('/movie/popular')
+  const fetchApiConfig =()=> {
+    fetchDataFromApi('/configuration')
     .then((res)=> {
       console.log(res);
-      dispatch(getApiConfiguration(res))
+
+      const  url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+        
+      }
+      dispatch(getApiConfiguration(url))
     })
     
   }
 
   return (
     <BrowserRouter>
+    {/* <Header/> */}
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/:mediaType/:id' element={<Details/>}/>
+        <Route path='/search/:query' element={<SearchResult/>}/>
+        <Route path='/explore/:mediaType' element={<Explore/>}/>
+        <Route path='*' element={<PageNotFound/>}/>
       </Routes>
+      {/* <Footer/> */}
     </BrowserRouter>
   )
 }
